bfafdbd4dc76f5d7d036ab79643b80f37e820647
kame211031 <arrowsuke@yahoo.co.jp>
2019-02-09 19:34:15 +0900

    FirstCommit

--- TwitterTLGetApp/ViewController.swift
+++ TwitterTLGetApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func TweetTwitter(_ sender: AnyObject) {
+        
+    }
+    
 }
 
